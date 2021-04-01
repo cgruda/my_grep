ae9373b7f8307cb8a76c3d3c7cc4c39f82bff467
cgruda <cgruda@gmail.com>
2021-04-02 00:44:48 +0300

    test files fix

--- test.py
+++ test.py
@@ -8,10 +8,12 @@ NC='\033[0m'
 
 test_idx = 1
 
+# run single test
 def test(path, pattern, options):
 
 	global test_idx
 
+	# build mygrep and grep commands
 	mygrep_command = "./mygrep"
 	grep_command = "grep"
 	pattern_path = " {} {}".format(pattern, path)
@@ -19,29 +21,35 @@ def test(path, pattern, options):
 	for opt in options:
 		options_str += " " + opt
 
+	# print log
 	log = YELLOW + "[%4d] " %test_idx + NC + mygrep_command + options_str + pattern_path
 	log = log.ljust(75)
 	print(log, end=' ')
 
+	# direct to an output file
 	mygrep_command += options_str + pattern_path + " > mygrep_output"
 	grep_command += options_str + pattern_path + " > grep_output"
 	
+	# run my_grep
 	mygrep_status = os.system(mygrep_command)
 	if (mygrep_status != 0):
 		print(RED + " [mygrep fail {}]".format(mygrep_status) + NC)
 		exit()
 
+	# run grep
 	grep_status = os.system(grep_command) >> 8
 	if (grep_status == 2):
 		print(RED + " [grep fail {}]".format(grep_status) + NC)
 		exit()
 
+	# comapre results - exit if no match
 	cmp_command = "cmp -s mygrep_output grep_output"
 	cmp_status = os.system(cmp_command)
 	if (cmp_status != 0):
 		print(RED + " [fail]" + NC)
 		exit()
 
+	# test passed - remove output files
 	os.system("rm mygrep_output")
 	os.system("rm grep_output")
 	print(GREEN + " [pass]" + NC)
@@ -49,10 +57,7 @@ def test(path, pattern, options):
 
 	return 0
 
-
-file0 = "file0"
-file1 = "file1"
-
+# create permutations of options for grep/mygrep
 def permutations_combinations(alist):
 	result = []
 	for i in range(len(alist)):
@@ -62,14 +67,17 @@ def permutations_combinations(alist):
 	return result
 
 
+file0 = "file0"
+file1 = "file1"
 options_arr = ["-A 3", "-b", "-c", "-i", "-n", "-v", "-x"]
 strs = ["dontfind", "chaim", "CHAIM", "\"is chaim\""]
 regex_strs = ["\"(my name is moshe|moshe)[h-m]\"", "\"\\\\text \"", "\"escaping \| chars\"", "\"(name is moshe|moshe)\"", "\"m[A-Z] name\"", "\"(CHAIM|moshe)\"", "\"m[a-z] na.e is (CHaim|moshe)\"", "\"m[a-z] na.e is \[]\"", "\"\[\]\"", "\"for\.\"", "\"\( some\)\""]
 opts = permutations_combinations(options_arr)
 
+# test an empty file (sanity)
 test(file0, "chaim", [])
 
-
+# run multiple tests different patterns and options
 for optper in opts:
 	for astr in strs:
 		result = test(file0, astr, optper)
--- test.sh
+++ test.sh
@@ -1,68 +1,10 @@
 #!/bin/sh
 
-TEST_IDX=1
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
-
-compile() {
-	gcc *.c -o mygrep
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-}
-
-all_test() {
-	/usr/bin/python3 /home/cgruda/LABC/Ex2/test.py
-# 	touch mygrep_output
-# 	touch grep_output
-
-# 	printf ${YELLOW}"[%2d] "${NC} $TEST_IDX
-# 	echo "./ex2 "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}""
-
-# 	# run mygrep and catch mygrep errors (return value non-zero)
-# 	./ex2 $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} > mygrep_output
-# 	STATUS=$?
-# 	if [ $STATUS -ne 0 ]; then
-# 		tput cuu 1
-# 		tput cuf 45
-# 		echo "${RED}[mygrep fail $STATUS]${NC}"
-# 		exit 1
-# 	fi
-
-# 	# run grep and catch grep errors (return value 2)
-# 	grep $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} > grep_output
-# 	STATUS=$?
-# 	if [ $STATUS -eq 2 ]; then
-# 		tput sc
-# 		tput cuu 2
-# 		tput cuf 45
-# 		echo "${RED}[grep fail $STATUS]${NC}"
-# 		tput rc
-# 		exit 1
-# 	fi
-
-# 	tput cuu 1
-# 	tput cuf 45
-
-# 	# compare grep output and mygrep output
-# 	cmp -s mygrep_output grep_output
-# 	if [ $? -ne 0 ]; then
-# 		echo "${RED}[fail]${NC}"
-# 		exit 1
-# 	else
-# 		rm mygrep_output
-# 		rm grep_output
-# 		echo "${GREEN}[pass]${NC}"
-# 	fi
-
-# 	TEST_IDX=$((TEST_IDX+1))
-}
-
 # compile
-compile
+gcc *.c -o mygrep
+if [ $? -ne 0 ]; then
+	exit 1
+fi
 
-# run tests
-all_test
-
+# run python script
+/usr/bin/python3 /home/cgruda/LABC/Ex2/test.py
